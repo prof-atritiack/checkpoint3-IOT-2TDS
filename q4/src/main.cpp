c1238e34ea3e65e5c2deeec07877643271a4d8d4
ander kamada <anderkamada3@gmail.com>
2025-05-15 16:09:34 -0300

    subindo parte do projeto

---
+++ q4/src/main.cpp
@@ -0,0 +1,16 @@
+// Identificadores
+const char* ID        = "GRUPO-123";
+const char* moduleID  = "ESP32-Q4";
+
+// Wi-Fi
+const char* SSID      = "Wokwi-GUEST";
+const char* PASSWORD  = "";
+
+// MQTT Broker
+const char* BROKER_MQTT  = "4.201.104.101"; 
+const int   BROKER_PORT  = 1883;
+const char* mqttUser     = "admin";         
+const char* mqttPassword = "";     
+
+// Tópico MQTT
+#define TOPICO_PUBLISH  "esp32/sensores"
